62e3078926e3387a731d9cef90b41410564b45d8
hoolek77 <szymon.kin@gmail.com>
2021-05-07 14:15:45 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
