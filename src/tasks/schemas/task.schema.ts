3e67e39be2237ea7aaa3c3a5233e5b8333ba73ff
hoolek77 <szymon.kin@gmail.com>
2021-05-09 21:57:55 +0200

    timestamps are no longer included in tasks

--- src/tasks/schemas/task.schema.ts
+++ src/tasks/schemas/task.schema.ts
@@ -1,31 +1,28 @@
 import * as mongoose from 'mongoose';
 
-export const TaskSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-    },
-    title: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    description: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    color: {
-      type: String,
-      minLength: 7,
-      maxLength: 7,
-      required: true,
-    },
-    finished: {
-      type: Boolean,
-      default: false,
-    },
+export const TaskSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
   },
-  { timestamps: true },
-);
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  color: {
+    type: String,
+    minLength: 7,
+    maxLength: 7,
+    required: true,
+  },
+  finished: {
+    type: Boolean,
+    default: false,
+  },
+});
